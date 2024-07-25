2106521f57d7379a7456902536c80658ed139515
MateooMoran <brandon.moran@epn.edu.ec>
2024-07-24 19:30:52 -0500

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String URL = "jdbc:mysql://sql10.freemysqlhosting.net:3306/sql10720943";
+        String USER = "sql10720943";
+        String PASSWORD = "CKzVnnXHi6";
+
+
+
+    }
+}
